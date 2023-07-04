954acb91d62cff646ccdf90d22ae833bbe66745b
armworkexp <138574635+armworkexp@users.noreply.github.com>
2023-07-04 10:27:34 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,18 @@
 radio.onReceivedNumber(function (receivedNumber) {
+    // Power efficiency, checks every millisecond instead of every microsecond
     control.waitMicros(1000)
+    // Play sound if closer than 1m.
     if (Math.abs(radio.receivedPacket(RadioPacketProperty.SignalStrength) + 42) < 11) {
         music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     }
+    // Log serial number of collision satellite for identification.
     datalogger.log(datalogger.createCV("nearCollisionSats", radio.receivedPacket(RadioPacketProperty.SerialNumber)))
 })
+// Send remote signal to tell satellite whether or not it's parachuting.
 input.onButtonPressed(Button.A, function () {
     radio.sendString("parachute")
 })
+// Log acceleration data during freefall in correct column.
 input.onGesture(Gesture.FreeFall, function () {
     basic.showIcon(IconNames.SmallSquare)
     if (parachuting == 1) {
@@ -17,6 +22,7 @@ input.onGesture(Gesture.FreeFall, function () {
     }
     basic.showIcon(IconNames.Yes)
 })
+// Determine whether parachuting or not from remote signal.
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "parachute") {
         parachuting = 1
@@ -30,9 +36,11 @@ input.onButtonPressed(Button.B, function () {
     radio.sendString("noParachute")
 })
 let parachuting = 0
+// Group 10 for collision detection
 radio.setGroup(10)
 parachuting = 1
 basic.showIcon(IconNames.Yes)
+// Initialize data collection for acceleration and sattelites that near collision
 datalogger.setColumnTitles(
 "accelerationPara",
 "accelerationNoPara",
